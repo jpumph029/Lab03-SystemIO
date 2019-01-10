78c88e9d74ab3aa47a658ea67f5397cedf7c0f4a
Joseph <pumphrey.joseph@gmail.com>
2019-01-10 10:23:19 -0800

    Set up Basic System.IO methods to create/delete/append/read. Set up unit testing.

---
+++ Word Guess Game/Word Guess Game Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Word_Guess_Game_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Word Guess Game/Word Guess Game/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+
+namespace Word_Guess_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../testFile.txt";
+            //DeleteFile(path);
+            CreateFile(path);
+            //AppendToFile(path);
+            ReadFile(path);
+            Console.Read();
+            Console.WriteLine("Hello World!");
+        }
+        static void CreateFile(string path)
+        {
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(path))
+                {
+                    try
+                    {
+                        //the text made with the file
+                        streamWriter.WriteLine("This is a test. Hello File");
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.Write(e);
+                throw;
+            }
+            catch (NotSupportedException e)
+            {
+
+                Console.Write(e);
+                throw;
+            }
+            catch (Exception e)
+            {
+
+                Console.Write(e);
+                throw;
+            }
+        }
+        static void ReadFile(string path)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    Console.WriteLine(lines[i]);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        static void AppendToFile(string path)
+        {
+            try
+            {
+                using (StreamWriter streamWriter = File.AppendText(path))
+                {
+                    streamWriter.WriteLine("This is text appened from AppendToFileMethod");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        static void DeleteFile(string path)
+        {
+            try
+            {
+                File.Delete(path);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
